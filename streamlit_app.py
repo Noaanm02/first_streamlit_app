e03f384a133a383fd05bded49dca8dc273d547a9
Noaanm02 <128167806+Noaanm02@users.noreply.github.com>
2023-03-17 15:26:35 -0600

    Update streamlit_app.py
    Fixe url name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.uswest-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
 
