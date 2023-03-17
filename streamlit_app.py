081e1cdf2b365fc38ee16ba78fc7e2cbfcedb664
Noaanm02 <128167806+Noaanm02@users.noreply.github.com>
2023-03-17 15:24:48 -0600

    Update streamlit_app.py
    added table for the menu specila option

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.uswest-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruitlist)
+
